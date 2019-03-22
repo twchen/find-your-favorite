e577b8d4d99a5fa2042433a573b93fbb84fdcd9f
Tianwen CHEN <tianwen.chen@hotmail.com>
2019-03-22 13:27:27 +0800

    publish webpage

---
+++ docs/precache-manifest.5f968fda788411f83ecd7d64f74a5805.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a412feca1a62a9b37e69",
+    "url": "./static/js/main.a412feca.chunk.js"
+  },
+  {
+    "revision": "29773969d8b3deac9549",
+    "url": "./static/js/1.29773969.chunk.js"
+  },
+  {
+    "revision": "7ce8e04ad49b993d11c5084de58a81bc",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8ede13ff351e4ab1636fc1073100578e.js"
+  "./precache-manifest.5f968fda788411f83ecd7d64f74a5805.js"
 );
 
 workbox.clientsClaim();
